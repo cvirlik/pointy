5c850d8805f50d3be00d4279a6c80ff7a9536d69
cvirlik <aglevickaa@gmail.com>
2024-08-01 17:32:22 +0200

    Add Lava Animation

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -1,19 +1,37 @@
-import { ScrollView, StyleSheet } from 'react-native';
-import type { View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+import { Lava } from './Lava';
 
 type PageProps = React.ComponentProps<typeof View> & {
   children?: React.ReactNode;
 };
 
 export function PageContainer(props: PageProps) {
-  return <ScrollView contentContainerStyle={styles.container}>{props.children}</ScrollView>;
+  return (
+    <View style={styles.container}>
+      <Lava style={styles.lava} />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>{props.children}</ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
     paddingHorizontal: 16,
-    paddingBottom: 32,
+    paddingBottom: 81,
     alignItems: 'center',
     gap: 32,
+  },
+  lava: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: -1,
   },
 });
