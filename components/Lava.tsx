8c64842bf724fe79b251251df5357f0d5e674389
cvirlik <aglevickaa@gmail.com>
2024-08-05 16:25:37 +0200

    feat: blob animation background

---
+++ components/Lava.tsx
@@ -0,0 +1,109 @@
+import Animated, {
+  useDerivedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+  withSequence,
+  Easing,
+} from 'react-native-reanimated';
+import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { useMemo } from 'react';
+import { BlurView } from 'expo-blur';
+
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function Lava() {
+  const { width, height } = useWindowDimensions();
+
+  const circles = useMemo(() => {
+    const rand = randomNumber(5, 12) / 10;
+    const radius = (width * rand) / 2;
+    return Array.from({ length: 6 }, (_, index) => ({
+      index,
+      x: Math.random() * (width - radius * 2),
+      y: Math.random() * (height - radius * 2),
+      radius,
+      color: '#72B8EB',
+    }));
+  }, [height, width]);
+
+  return (
+    <View style={StyleSheet.absoluteFillObject}>
+      {circles.map(circle => (
+        <Circle
+          key={circle.index}
+          color={circle.color}
+          radius={circle.radius}
+          x={circle.x}
+          y={circle.y}
+        />
+      ))}
+      <BlurView
+        intensity={100}
+        experimentalBlurMethod="dimezisBlurView"
+        style={[StyleSheet.absoluteFillObject]}
+      />
+    </View>
+  );
+}
+
+interface CircleProps {
+  color: string;
+  x: number;
+  y: number;
+  radius: number;
+}
+
+const Circle: React.FC<CircleProps> = ({ color, radius, x, y }) => {
+  const randRotation = Math.random() * 360;
+
+  const rotation = useDerivedValue(() => {
+    return withRepeat(
+      withSequence(
+        withTiming(randRotation, { duration: 0 }),
+        withTiming(randRotation + 360, { duration: 10000, easing: Easing.linear }),
+      ),
+      -1,
+      false,
+    );
+  }, []);
+
+  const styles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: `${rotation.value}deg`,
+        },
+      ],
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles,
+        {
+          transformOrigin: ['50%', y, 0],
+        },
+      ]}
+    >
+      <View
+        style={[
+          {
+            backgroundColor: color,
+            width: radius * 2,
+            height: radius * 2,
+            position: 'absolute',
+            left: x - radius,
+            top: y - radius,
+            borderRadius: radius,
+            opacity: 0.6,
+          },
+        ]}
+      />
+    </Animated.View>
+  );
+};
--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -9,41 +9,21 @@ type PageProps = React.ComponentProps<typeof View> & {
 export function PageContainer(props: PageProps) {
   return (
     <View style={styles.container}>
-      <Lava style={styles.lava} />
+      <Lava />
       <ScrollView contentContainerStyle={styles.scrollContainer}>{props.children}</ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  //   container: {
-  //     backgroundColor: 'white',
-  //     paddingHorizontal: 16,
-  //     paddingBottom: 81,
-  //     alignItems: 'center',
-  //     gap: 32,
-  //   },
   container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  scrollContainer: {
     flex: 1,
     backgroundColor: 'white',
   },
   scrollContainer: {
     paddingHorizontal: 16,
     paddingBottom: 81,
-    paddingBottom: 81,
     alignItems: 'center',
     gap: 32,
-  },
-  lava: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: -1,
   },
 });
